67a0fc446105d5e416e3e9c50c333a2c12b5328b
REETU SINGH <reetu2624@gmail.com>
2021-04-22 18:04:54 -0400

    map updation based on attribute selection

--- country.js
+++ country.js
@@ -1,44 +1,37 @@
 var format = d3.format(",");
 var selected_feature;
-
+var dataUrl;
 
 var url="http://127.0.0.1:5000/get_map_data/"
+var colorScale="http://127.0.0.1:5000/get_color_map/"
 
 async function getJson(dataUrl){
     var response = await fetch(dataUrl,
       {
         method: "GET",
-        mode: 'no-cors',
+        // mode: 'no-cors',
         headers: {
           'Content-Type': 'application/json',
           "Access-Control-Allow-Origin" : "*", 
           "Access-Control-Allow-Credentials" : true 
         }
     });
-    var data= await response.json();
-    console.log(data)
-
-    return data;
+    return await response.json();
 }
 
-
-// function tip(optionss){
-    var tip = d3.tip()
+ var tip = d3.tip()
             .attr('class', 'd3-tip')
             .offset([-10, 0])
-            .html(function(d) {
-              return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong> Happiness Score: </strong><span class='details'>" + format(d.Score) +"</span>";
+            .html(function(d,name) {
+              return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>"+ name +": </strong><span class='details'>" + format(d[name]) +"</span>";
             })
-    // return tip
-
-// }
 
 var margin = {top: -40, right: 0, bottom: 0, left: 0},
             width = 800 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
 
 var color = d3.scaleThreshold()
-    .domain([0,2,3,4,5,6,7,8,9,10])
+    .domain([1,2,3,4,5,6,7,8,9,10])
     .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)","rgb(33,113,181)","rgb(8,81,156)","rgb(8,48,107)","rgb(3,19,43)"]);
 
 var path = d3.geoPath();
@@ -62,38 +55,48 @@ d3.queue()
     .defer(d3.json, "world_countries.json")
     .await(ready);
 
-
-
 async function ready(error, data) {
 
-    dataUrl=url
-    dataUrl+="Score"
-    console.log(dataUrl)
-    var csvdata=await getJson(dataUrl);  
-
-    var options=["Select","Score","Rank","GDP per capita","Social support", "Healthy life expectancy","Freedom to make life choices","Generosity",
-    "Perceptions of corruption","Population","Percentage of internet users","percentage of non-religious people"]
+    var options=["Select","Score","Overall rank","GDP per capita","Social support", "Healthy life expectancy","Freedom to make life choices","Generosity",
+    "Perceptions of corruption","Population","Percentage","percentage_non_religious"]
     
-    // d3.select("#menu")
-    //       .selectAll('myOptions')
-    //          .data(options)
-    //       .enter()
-    //         .append('option')
-    //       .text(function (d) { return d; }) // text showed in the menu
-    //       .attr("value", function (d) { return d; })
+    d3.select("#menu")
+          .selectAll('myOptions')
+            .data(options)
+          .enter()
+            .append('option')
+          .text(function (d) { return d; }) // text showed in the menu
+          .attr("value", function (d) { return d; })
     
 
-    // d3.select("#menu").on("change", function(){
-    //     selected_feature=d3.select("#menu").property("value")
-    //     console.log(selected_feature)
-    // } )
+    d3.select("#menu").on("change", function(){
+        selected_feature=d3.select("#menu").property("value");
+        displayMap(selected_feature)    
+    } )
 
+    async function displayMap(feature){
 
-    // function displayMap(option){
+      dataUrl=url
+      dataUrl+=feature
+      colorUrl=colorScale+feature
+      console.log(colorUrl)
 
+      var csvdata=await getJson(dataUrl);  
+      var colordata=await getJson(colorUrl);
+      
         var optionByCountry={};
-        csvdata.forEach(function (d)  { optionByCountry[d.iso]=+d.Score; });
-        data.features.forEach(function(d) { d.Score = optionByCountry[d.id] });
+        var colorVal={};
+        
+        csvdata.forEach(function (d)  { 
+          optionByCountry[d.iso]=+d[feature]; 
+        });
+
+        colordata.forEach(function (d)  { 
+          colorVal[d.iso]=d[feature+"_scaled"]; 
+        });
+        console.log(colorVal)
+
+        data.features.forEach(function(d) { d[feature] = optionByCountry[d.id] });
         svg.append("g")
           .attr("class", "countries")
         .selectAll("path")
@@ -101,7 +104,7 @@ async function ready(error, data) {
           .enter().append("path")
           .attr("d", path)
             .style("fill", function(d) { 
-              return color(optionByCountry[d.id]); })
+              return color(colorVal[d.id]); })
             .style('stroke', 'white')
             .style('stroke-width', 1.5)
             .style("opacity",0.8)
@@ -110,14 +113,14 @@ async function ready(error, data) {
           .style('stroke-width', 0.3)
         .on('mouseover',function(d){
 
-            tip.show(d);
+            tip.show(d,feature);
           d3.select(this)
             .style("opacity", 1)
             .style("stroke","white")
             .style("stroke-width",3)
         })
         .on('mouseout', function(d){
-            tip.hide(d);
+            tip.hide(d,feature);
 
           d3.select(this)
             .style("opacity", 0.8)
@@ -132,14 +135,6 @@ async function ready(error, data) {
 
         
     }
-
-    // function display(){
-    //     var selected = d3.select("#menu").property("value");
-    //     if (selected == "Rank"){
-    //         displayMap("Overall rank")
-    //     }
-                
-    // }
-// }
+}
 
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 import flask
 import pandas as pd
 from flask import render_template, request
-
+from sklearn import preprocessing
 app = Flask(__name__)
 
 
@@ -15,7 +15,7 @@ def hello_world():
 @app.route('/get_map_data/<string:feature>')
 def get_map_data(feature):
     data = pd.read_csv("final.csv")
-    result_df = data[['country', feature]]
+    result_df = data[['country','iso', feature]]
     return result_df.to_json(orient='records')
     # with open('data/final.csv') as csv_file:
     #     data = csv.reader(csv_file, delimiter=',')
@@ -53,6 +53,24 @@ def get_religion_data(feature):
     data = pd.read_csv("final.csv")
     result_df = data[['country', 'percentage_non_religious', feature]]
     return result_df.to_json(orient='records')
+
+
+# x = df.values #returns a numpy array
+# min_max_scaler = preprocessing.MinMaxScaler()
+# x_scaled = min_max_scaler.fit_transform(x)
+# df = pd.DataFrame(x_scaled)
+
+
+@app.route('/get_color_map/<string:feature>')
+def get_color_map(feature):
+    data = pd.read_csv("final.csv")
+    result_df = data[feature]
+    maximum=result_df.max()
+    minimum=result_df.min()
+    result_df=((result_df-minimum)/(maximum-minimum))*9+1
+    jsonObj=data[['country','iso']]
+    jsonObj[feature+"_scaled"]=result_df
+    return jsonObj.to_json(orient='records')
 
 if __name__ == "__main__":
     app.run(debug=True)
