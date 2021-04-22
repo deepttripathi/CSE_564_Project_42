9283391c514585ebd58754f873cb6195fa0f6e4c
Deept Tripathi <deepttripathi@gmail.com>
2021-04-21 21:55:50 -0400

    used pandas for data processing

--- data-prep/mergingCSV.py
+++ data-prep/mergingCSV.py
@@ -2,20 +2,23 @@ import pandas as pd
 
 
 def mergeCSV(basedir, filename1, filename2, filename3, onColumn):
-    data1 = pd.read_csv(basedir+filename1)
-    data2 = pd.read_csv(basedir+filename2).drop([])
-    data3 = pd.read_csv(basedir+filename3)
+    data1 = pd.read_csv(basedir + filename1)
+    data2 = pd.read_csv(basedir + filename2).drop([])
+    data3 = pd.read_csv(basedir + filename3)
     output1 = pd.merge(data1, data2, on=onColumn, how='inner')
     print(output1)
-    output2 = pd.merge(output1, data3, on=onColumn, how='inner').drop(['Unnamed: 0', 'iso', 'gdp_pc', 'gpi_rank', 'gpi_score'], axis=1)
+    output2 = pd.merge(output1, data3, on=onColumn, how='inner').drop(
+        ['Unnamed: 0', 'iso', 'gdp_pc', 'gpi_rank', 'gpi_score'], axis=1)
     print(output2)
     return output2
 
+
 def main():
     df = mergeCSV('../data/', 'happiness.csv',
-             'InternetUsers.csv', 'relig_iso.csv', 'country')
+                  'InternetUsers.csv', 'relig_iso.csv', 'country')
 
     df.to_csv('../data/final.csv')
+
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask
-import csv
-import json
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -12,22 +11,45 @@ def hello_world(name):
 
 @app.route('/get_map_data/<string:feature>')
 def get_map_data(feature):
-    with open('data/final.csv') as csv_file:
-        data = csv.reader(csv_file, delimiter=',')
-        print(data)
-        first_line = True
-        result = []
-        feature_index = 0
-        for row in data:
-            if first_line:
-                feature_index = row.index(feature)
-                first_line = False
-            else:
-                result.append({
-                    "country": row[2],
-                    feature: row[feature_index]
-                })
-    return json.dumps(result)
+    data = pd.read_csv("data/final.csv")
+    result_df = data[['country', feature]]
+    return result_df.to_json(orient='records')
+    # with open('data/final.csv') as csv_file:
+    #     data = csv.reader(csv_file, delimiter=',')
+    #     first_line = True
+    #     result = []
+    #     feature_index = 0
+    #     for row in data:
+    #         if first_line:
+    #             feature_index = row.index(feature)
+    #             first_line = False
+    #         else:
+    #             result.append({
+    #                 "country": row[2],
+    #                 feature: row[feature_index]
+    #             })
+    # return json.dumps(result)
+
+
+@app.route('/get_bar_chart_data/<string:country>')
+def get_bar_chart_data(country):
+    data = pd.read_csv("data/final.csv")
+    country_data = data[data['country'] == country]
+    return country_data.to_json(orient='records')
+
+
+@app.route('/get_internet_data/<string:feature>')
+def get_internet_data(feature):
+    data = pd.read_csv("data/final.csv")
+    result_df = data[['country', 'Percentage', feature]]
+    return result_df.to_json(orient='records')
+
+
+@app.route('/get_religion_data/<string:feature>')
+def get_religion_data(feature):
+    data = pd.read_csv("data/final.csv")
+    result_df = data[['country', 'percentage_non_religious', feature]]
+    return result_df.to_json(orient='records')
 
 
 if __name__ == "__main__":
