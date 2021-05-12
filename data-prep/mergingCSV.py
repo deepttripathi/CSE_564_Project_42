eb91ba68a3c8829ad5c7ac5736bcf5368386006f
Deept Tripathi <deepttripathi@gmail.com>
2021-05-12 15:37:13 -0400

    scatterplot and pie chart js files added

--- data-prep/mergingCSV.py
+++ data-prep/mergingCSV.py
@@ -10,31 +10,19 @@ def mergeCSV(basedir, filename1, filename2, filename3, filename4, onColumn):
     # print(output1)
     output2 = pd.merge(output1, data3, on=onColumn, how='outer').drop(
         ['Unnamed: 0', 'gdp_pc', 'gpi_rank', 'gpi_score'], axis=1)
-<<<<<<< HEAD
     # print(output2)
-    return output2
-=======
-    print(output2)
     output3 = pd.merge(output2, data4, on=onColumn, how='outer')
-    print(output3)
     return output3
->>>>>>> dd6531d7aef48b2eda460dc4770960e3d0d7d718
-
-
 
 
 def main():
     df = mergeCSV('../data/', 'happiness.csv',
-<<<<<<< HEAD
-                  'InternetUsers.csv', 'relig_iso.csv', 'country')
-    temp=df['Percentage'].str.replace('%','').astype(float)
-    df['Percentage']=temp
-=======
                   'InternetUsers.csv', 'relig_iso.csv', 'regional-ind.csv', 'country')
->>>>>>> dd6531d7aef48b2eda460dc4770960e3d0d7d718
+    temp = df['Percentage'].str.replace('%', '').astype(float)
+    df['Percentage'] = temp
 
-    temp1=df['Population'].str.replace(',','').astype(float)
-    df['Population']=temp1
+    temp1 = df['Population'].str.replace(',', '').astype(float)
+    df['Population'] = temp1
     print(df['Population'])
     df.to_csv('../data/final.csv')
 
